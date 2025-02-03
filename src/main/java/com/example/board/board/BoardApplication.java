5fafd92ccb3a7a5bb7655cbcc89577b6005fc2cd
luail <kynia0506@naver.com>
2025-02-03 10:24:43 +0900

    02.03 study

--- src/main/java/com/example/board/board/BoardApplication.java
+++ src/main/java/com/example/board/board/BoardApplication.java
@@ -3,7 +3,11 @@ package com.example.board.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+
+//세션스토리지로 redis를 사용하겠다라는 설정
+@EnableRedisHttpSession
 @SpringBootApplication
 @EnableScheduling //스케줄러 사용 시 필요한 설정.
 public class BoardApplication {
